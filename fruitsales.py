815b4b1750ed0be7fecf52f6f654546bfb4f6338
Clare E <136653618+clareegan@users.noreply.github.com>
2023-08-30 14:43:42 -0400

    Update fruitsales.py

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,6 @@
-# add your code here
+fruit_sales = pd.DataFrame({'Apples': [35, 41],
+                            'Bananas': [21, 34]},
+                            index=['2017 Sales', '2018 Sales'])
+
+q2.check()
+fruit_sales
